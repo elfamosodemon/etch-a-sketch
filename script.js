a9598457ff2a236cf1be920f58d6d614c448d9dc
Leo <leodasilva.2012@hotmail.com>
2022-05-05 20:49:39 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const container = document.querySelector('.container');
+const grid_64 = document.querySelector('.grid_64');
+const grid_16 = document.querySelector('.grid_16');
+
+
+function makeGrid(a, b) {
+    for (let i = 0; i < a * b; i++) {
+        const grid_div = document.createElement('div');
+        grid_div.classList.toggle('box');
+        grid_div.setAttribute('id', 'grids')
+        container.appendChild(grid_div);
+        if (a != 16 && b != 16) {
+            grid_div.classList.toggle('boxTransform');
+
+        }
+    }
+}
+
+function colorChange() {
+    const grid_div = document.querySelectorAll('.box');
+    grid_div.forEach(grid_div => grid_div.addEventListener('mouseover', function(e) {
+        grid_div.classList.add('colorChange')
+    }));
+
+}
+
+function changeGrid() {
+    grid_64.addEventListener('click', function() {
+        document.querySelectorAll('.box').forEach(e => e.remove())
+        makeGrid(64, 64)
+        colorChange()
+
+    });
+    grid_16.addEventListener('click', function() {
+        document.querySelectorAll('.box').forEach(e => e.remove())
+        makeGrid(16, 16)
+        colorChange()
+    });
+
+}
+
+changeGrid()
+makeGrid(16, 16)
+colorChange()
