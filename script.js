b49b9d8ad5575f400fb3515c328db12199435f8d
Leo <leodasilva.2012@hotmail.com>
2022-05-09 14:57:34 -0300

    now user can choose grid size and brush color

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-const container = document.querySelector('.container');
-const grid_64 = document.querySelector('.grid_64');
-const grid_16 = document.querySelector('.grid_16');
+let color = 'black';
 
+function makeGrid(size) {
+    let grid = document.querySelector('.grid');
+    let box = grid.querySelectorAll('div');
+    box.forEach((div) => div.remove());
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-function makeGrid(a, b) {
-    for (let i = 0; i < a * b; i++) {
-        const grid_div = document.createElement('div');
-        grid_div.classList.toggle('box');
-        grid_div.setAttribute('id', 'grids')
-        container.appendChild(grid_div);
-        if (a != 16 && b != 16) {
-            grid_div.classList.toggle('boxTransform');
+    let total = size * size;
+    for (let i = 0; i < total; i++) {
+        let box = document.createElement('div');
+        box.addEventListener('mouseover', (colorChange));
+        box.style.backgroundColor = 'white';
+        grid.insertAdjacentElement("beforeend", box);
 
-        }
     }
 }
 
+makeGrid(16);
+
+function newGridSize(input) {
+    if (input >= 2 && input <= 100) {
+        makeGrid(input);
+    } else {
+        console.log("greater than 2, less than 100")
+    }
+
+}
+
 function colorChange() {
-    const grid_div = document.querySelectorAll('.box');
-    grid_div.forEach(grid_div => grid_div.addEventListener('mouseover', function(e) {
-        grid_div.classList.add('colorChange')
-    }));
-
+    if (color === 'random') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
 }
 
-function changeGrid() {
-    grid_64.addEventListener('click', function() {
-        document.querySelectorAll('.box').forEach(e => e.remove())
-        makeGrid(64, 64)
-        colorChange()
-
-    });
-    grid_16.addEventListener('click', function() {
-        document.querySelectorAll('.box').forEach(e => e.remove())
-        makeGrid(16, 16)
-        colorChange()
-    });
-
+function newColor(choice) {
+    color = choice;
 }
 
+function resetGrid() {
+    let grid = document.querySelector('.grid');
+    let box = grid.querySelectorAll('div');
+    box.forEach((div) => div.style.backgroundColor = 'white');
 
-
-changeGrid()
-makeGrid(16, 16)
-colorChange()
+}
